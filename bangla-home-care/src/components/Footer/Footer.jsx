87ad697ec41ccc5a612bd153c5250a0959ec8cbc
foysal90 <foysal1416@hotmail.com>
2025-07-29 21:17:24 -0400

    lang support  added for footer

--- bangla-home-care/src/components/Footer/Footer.jsx
+++ bangla-home-care/src/components/Footer/Footer.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { useTranslation } from "react-i18next";
+import { FaTwitter, FaYoutube, FaFacebook } from "react-icons/fa";
 const Footer = () => {
+  const { t } = useTranslation();
+
   return (
     <footer className="relative bg-gray-900 text-white pt-24">
       {/* Wave Background */}
@@ -30,54 +33,58 @@ const Footer = () => {
       </div>
 
       {/* Footer Content */}
-      <div className="relative z-10 footer sm:footer-horizontal  text-white p-10">
+      <div className="relative z-10 footer sm:footer-horizontal text-white p-10">
+        {/* Office Hours */}
         <nav>
-          <h6 className="footer-title">Office Hours</h6>
+          <h6 className="footer-title">{t("translation.officeHours")}</h6>
           <Link className="block mt-2 cursor-default" to="#">
-            Monday: 9:00 AM - 5:00 PM
+            {t("translation.monday")}
           </Link>
           <Link className="block cursor-default" to="#">
-            Tuesday: 9:00 AM - 5:00 PM
+            {t("translation.tuesday")}
           </Link>
           <Link className="block cursor-default" to="#">
-            Wednesday: 9:00 AM - 5:00 PM
+            {t("translation.wednesday")}
           </Link>
           <Link className="block cursor-default" to="#">
-            Thursday: 9:00 AM - 5:00 PM
+            {t("translation.thursday")}
           </Link>
           <Link className="block cursor-default" to="#">
-            Friday: 9:00 AM - 5:00 PM
+            {t("translation.friday")}
           </Link>
           <Link className="block cursor-default" to="#">
-            Saturday: Closed
+            {t("translation.saturday")}
           </Link>
           <Link className="block cursor-default" to="#">
-            Sunday: Closed
-          </Link>
-        </nav>
-        <nav>
-          <h6 className="footer-title">Services</h6>
-          <Link className="link link-hover" to="#">
-            Personal Care
-          </Link>
-          <Link className="link link-hover" to="#">
-            Companion Care
-          </Link>
-          <Link className="link link-hover" to="/">
-            Respite Care
-          </Link>
-          <Link className="link link-hover" to="#">
-            Specialized Care
+            {t("translation.sunday")}
           </Link>
         </nav>
 
+        {/* Services */}
         <nav>
-          <h6 className="footer-title">Company</h6>
+          <h6 className="footer-title">{t("translation.services")}</h6>
+          <Link className="link link-hover" to="/service/personal-care">
+            {t("translation.personalCare")}
+          </Link>
+          <Link className="link link-hover" to="/service/companionship">
+            {t("services.companionship.name")}
+          </Link>
+          <Link className="link link-hover" to="/service/meal-preparation">
+            {t("services.mealPrep.name")}
+          </Link>
+          <Link className="link link-hover" to="/service/medication-reminder">
+            {t("services.medication.name")}
+          </Link>
+        </nav>
+
+        {/* Company */}
+        <nav>
+          <h6 className="footer-title">{t("translation.company")}</h6>
           <Link className="link link-hover" to="#">
-            About us
+            {t("translation.aboutUs")}
           </Link>
           <Link className="link link-hover" to="#">
-            Contact
+            {t("translation.contact")}
           </Link>
           <Link className="link link-hover" to="#">
             Jobs
@@ -87,44 +94,18 @@ const Footer = () => {
           </Link>
         </nav>
 
+        {/* Social */}
         <nav>
-          <h6 className="footer-title">Social</h6>
+          <h6 className="footer-title">{t("translation.social")}</h6>
           <div className="grid grid-flow-col gap-4">
             <Link to="#">
-              {/* Twitter Icon SVG */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-              </svg>
+              <FaTwitter size={24} className="fill-current" />
             </Link>
             <Link to="#">
-              {/* YouTube Icon SVG */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-              </svg>
+              <FaYoutube size={24} className="fill-current" />
             </Link>
             <Link to="#">
-              {/* Facebook Icon SVG */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-              </svg>
+              <FaFacebook size={24} className="fill-current" />
             </Link>
           </div>
         </nav>
