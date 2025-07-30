f33b7c8f2cf47dda1c00a0777a1fd823aca06b01
foysal90 <foysal1416@hotmail.com>
2025-07-29 21:32:42 -0400

    map  implemented

--- bangla-home-care/src/components/Contact/Contact.jsx
+++ bangla-home-care/src/components/Contact/Contact.jsx
@@ -26,6 +26,20 @@ const ContactUs = () => {
               <h3 className="text-xl font-semibold mb-2">Email</h3>
               <p className="text-gray-400">info@homecare.com</p>
             </div>
+
+            {/* Map embedded here */}
+            <div className="mt-8 rounded-lg overflow-hidden shadow-lg">
+              <iframe
+                title="Warren Michigan Map"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2948.233105889349!2d-83.0142617845405!3d42.491022779188236!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824d7b96467c86d%3A0x6c860399040a5f3a!2sWarren%2C%20MI%2048092!5e0!3m2!1sen!2sus!4v1690723928579!5m2!1sen!2sus"
+                width="100%"
+                height="250"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
           </div>
 
           {/* Contact Form */}
